4f4403c73b57ec961ee53816bf242f4b915e45f6
Ross MacArthur <ross@macarthur.io>
2021-07-01 15:17:39 +0200

    Improve documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,27 @@
-//! A more compact, user friendly implementation of `Cow`.
-//!
-//! - Optimized for use on 64-bit Unix systems (behind `unix` feature).
-//! - Much better support for `Cow<Path>` and `Cow<OsStr>`.
+//! A more compact, user friendly clone-on-write smart pointer.
 //!
 //! ```
 //! use std::path::Path;
 //! use dairy::Cow;
 //!
-//! let name: Cow<str> = "Hello World!".into();
-//! let path: Cow<Path> = "./path/to/file.txt".into();
+//! let borrowed: Cow<str> = Cow::borrowed("Hello World!");
+//! let owned: Cow<str> = Cow::owned(String::from("Hello World!"));
 //! ```
 //!
-//! ### Caveats
+//! [`dairy::Cow`][Cow] is an improved version of the standard library
+//! [`std::borrow::Cow`]. It is just 2 words wide, storing the length, capacity,
+//! and the ownership tag all in one word.
 //!
-//! - Only `str`, `OsStr`, `CStr` and `Path` types are supported.
-//! - `OsStr`, `CStr` and `Path` are only supported on Unix.
-//! - On 32-bit operating systems the maximum length of types is (2¹⁶ - 1).
+//! [`dairy::Cow`][Cow] has many more [`From`] and [`PartialEq`] implementations.
+//! Most notably for `Cow<Path>` making `Into<Cow<Path>>` just as nice to use as
+//! `Cow<str>`.
+//!
+//! Unfortunately these benefits come with some caveats:
+//!
+//! - Only `str`, `[T]`, `OsStr`, `CStr` and `Path` types are supported.
+//! - Additionally, `OsStr` and `Path` are only supported on Unix.
+//! - On 32-bit operating systems the maximum length is `u16::MAX` which is
+//!   not sufficient for all use cases.
 //!
 //! ## Acknowledgements
 //!
